096bdee2c882f57b13778a1fff84f3ffee31dff6
Myriam <myriam@duckstance.com>
2015-06-04 13:21:53 +0200

    off-canvas

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('#nav').onePageNav();
 
     $('a[href^="http"]').attr('target','_blank');
+    
+    $('.toggle').click(function(){
+        $('.overview').toggleClass('open');
+    });
 });
